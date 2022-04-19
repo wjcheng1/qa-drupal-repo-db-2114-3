d72e3cf1e0a74dde8fc9031baa05b4c2d9cef67c
wjcheng1 <>
2022-04-19 00:40:40 +0000

    :sparkles: Initial Commit

---
+++ CHANGELOG.md
@@ -0,0 +1,29 @@
+# @pantheon-systems/next-drupal-starter
+
+## 1.0.4
+
+### Patch Changes
+
+- 8a13bf2: [next-drupal] Remove postinstall script, refine readme for .env configuration
+
+## 1.0.3
+
+### Patch Changes
+
+- 9f7bc9a: [next-drupal] Change .env.local to .env.development.local so it does not load in production
+
+## 1.0.2
+
+### Patch Changes
+
+- 8b69fad: Further refined env variable config
+- f9974ac: Update READMEs and package.json metadata
+- Updated dependencies [f9974ac]
+  - @pantheon-systems/drupal-kit@1.0.2
+
+## 1.0.1
+
+### Patch Changes
+
+- 81e00e7: Add package-lock.json to gatsby-wordpress-starter and next-drupal-starter for distribution.
+- e304470: Reconfigure env vars so empty BACKEND_URL does not fail builds on infra
---
+++ next.config.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const getLocales = require("./scripts/get-locales");
+
+// Load the .env file for local development
+// .env.development.local by default
+require("dotenv").config({
+  path: path.resolve(process.cwd(), ".env.development.local"),
+});
+
+let backendUrl, imageDomain;
+if (process.env.BACKEND_URL === undefined) {
+  backendUrl = `https://${process.env.PANTHEON_CMS_ENDPOINT}`;
+  imageDomain = process.env.IMAGE_DOMAIN || process.env.PANTHEON_CMS_ENDPOINT;
+
+  // populate BACKEND_URL as a fallback and for build scripts
+  process.env.BACKEND_URL = `https://${process.env.PANTHEON_CMS_ENDPOINT}`;
+} else {
+  backendUrl = process.env.BACKEND_URL;
+  imageDomain =
+    process.env.IMAGE_DOMAIN ||
+    process.env.BACKEND_URL.replace(/^https:\/\//, "");
+}
+// remove trailing slash if it exists
+imageDomain = imageDomain.replace(/\/$/, "");
+
+module.exports = async () => {
+  const nextConfig = {
+    env: {
+      backendUrl: backendUrl,
+      // set imageUrl if IMAGE_DOMAIN is set in env vars to override default
+      imageUrl: `https://${imageDomain}`,
+    },
+    reactStrictMode: true,
+    images: {
+      domains: [imageDomain],
+    },
+    i18n: {
+      locales: await getLocales(),
+      defaultLocale: "en",
+    },
+    async rewrites() {
+      return [
+        {
+          // inline-images can still be fetched from their source
+          source: "/sites/default/:path*",
+          destination: `${backendUrl}/sites/default/:path*`,
+        },
+      ];
+    },
+    // In order to have the same layout data on each page, we need to fetch the data at buildtime
+    // and save it to a local file.
+    // see https://maxkarlsson.dev/blog/layout-in-next-js-from-external-source for more info
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        require("./scripts/generate-layout")();
+      }
+      return config;
+    },
+  };
+
+  return nextConfig;
+};
